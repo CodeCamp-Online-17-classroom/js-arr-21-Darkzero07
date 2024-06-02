84692c2a1f2f7e9b59de7b5f2a2cacccafbc2881
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-03 01:24:18 +0700

    js lab 107

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+function checkPermission(role, isAdminAction, isNotAdminAction) {
+  if (role === "ADMIN") isAdminAction();
+  else isNotAdminAction();
+}
+
+function isAdminAction() {
+ alert("ACCESS GRANTED");
+//   console.log("ACCESS GRANTED");
+}
+
+function isNotAdminAction() {
+  alert("ACCESS DENIED");
+//   console.log("ACCESS DENIED");
+}
+
+checkPermission("ADMIN", isAdminAction, isNotAdminAction);
+checkPermission("USER", isAdminAction, isNotAdminAction);
